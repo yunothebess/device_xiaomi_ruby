cfc2ad8a563bd83129ef291d3289a2b05e34a598
Kasun Herath <kasunherath@protonmail.com>
2023-11-30 15:17:45 +0530

    Revert "ruby: Switch back to OSS NFC stack"
    This reverts commit eed6bbf91881c5df0eaeedbec6822932f6a3533f.

--- device.mk
+++ device.mk
@@ -205,7 +205,7 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc-service.st \
+    android.hardware.nfc@1.2.vendor \
     com.android.nfc_extras \
     NfcNci \
     SecureElement \
